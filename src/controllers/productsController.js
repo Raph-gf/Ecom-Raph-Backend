e43f896189aa3e3af54622ea953cca500f231264
Raphael <raphaelgarnier1997@gmail.com>
2024-03-25 18:42:27 +0100

    deleted node_module & build from product controller

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -59,52 +59,7 @@ export const deleteProduct = async (req, res) => {
     res.status(500).json(error.message);
   }
 };
-// export const addProductToCart = async (req, res) => {
-//   try {
-//     // const user = await User.findById({_id:req.params._id});
-//     // console.log(user);
-//     const product = await Product.findById({ _id: req.params.id });
-//     console.log(product);
-//     if (!product) {
-//       console.log("erreur product not found");
-//     }
-//     const newcart = await Cart.create({ products: [product] });
-//     console.log(newcart);
-//     // newcart.products.push(product);
-//     newcart.save();
-//     res.json({ cart: newcart, message: "Product added successfully" });
-//   } catch (error) {
-//     res.json({ error: error.message });
-//   }
-// };
-// export const addProductToCart = async (req, res) => {
-//   try {
-//     const product = await Product.findById({ _id: req.params.id });
-//     if (!product) {
-//       return res.status(404).json({ error: "Product not found" });
-//     }
 
-//     const user = await User.findById(req.params.userId);
-//     if (!user) {
-//       return res.status(404).json({ error: "User not found" });
-//     }
-
-//     let cart = await Cart.findOne({ userId: user._id });
-
-//     if (!cart) {
-//       cart = await Cart.create({ userId: user._id, products: [product] });
-//       console.log("New cart created, product added succefully", cart);
-//     } else {
-//       cart.products.push(product);
-//       await cart.save();
-//       console.log("Product added to existing cart:", cart);
-//     }
-
-//     res.json({ cart, message: "Product added successfully" });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
 export const addProductToCart = async (req, res) => {
   try {
     const product = await Product.findById({ _id: req.params.id });
